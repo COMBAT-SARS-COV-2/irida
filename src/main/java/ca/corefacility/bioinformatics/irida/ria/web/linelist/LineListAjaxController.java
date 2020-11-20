6e65b77c242d33f8beff6d8826325ad46ecf2567
Josh Adam <josh.adam@canada.ca>
2020-11-20 11:12:50 -0600

    Added AJAX controller for all things linelist

---
+++ src/main/java/ca/corefacility/bioinformatics/irida/ria/web/linelist/LineListAjaxController.java
@@ -0,0 +1,12 @@
+package ca.corefacility.bioinformatics.irida.ria.web.linelist;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * AJAX Controller for all things Project > Linelist
+ */
+@RestController
+@RequestMapping("/ajax/projects/{projectId}/linelist")
+public class LineListAjaxController {
+}
